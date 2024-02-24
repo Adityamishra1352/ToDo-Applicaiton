143d985d0f3f09235f011b1a9e1055c6956828d0
Aditya Mishra <mishra.aditya1352@gmail.com>
2024-02-24 08:59:26 +0530

    Sreach for the teasks and functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './header.component.html',
   styles: ''
 })
 export class HeaderComponent {
-
+  stateService=inject(StateService);
+  searchControl=new FormControl();
+  ngOnInit(){
+    this.searchControl.valueChanges.subscribe((value)=>{
+      // console.log(value);
+      this.stateService.searchSubject.next(value || "");
+    })
+  }
 }
--- src/app/components/pages/all-tasks/all-tasks.component.ts
+++ src/app/components/pages/all-tasks/all-tasks.component.ts
@@ -4,6 +4,7 @@ import { HttpService } from '../../../services/http.service';
 import { DatePipe } from '@angular/common';
 import { PageTitleComponent } from '../../page-title/page-title.component';
 import { TaskListComponent } from '../../task-list/task-list.component';
+import { StateService } from '../../../services/state.service';
 
 @Component({
   selector: 'app-all-tasks',
@@ -14,9 +15,18 @@ import { TaskListComponent } from '../../task-list/task-list.component';
 })
 export class AllTasksComponent {
   newTask="";
+  initialtaskList:any[]=[];
   taskList:any[]=[];
   httpService=inject(HttpService);
+  stateService=inject(StateService);
   ngOnInit(){
+    this.stateService.searchSubject.subscribe((value)=>{
+      if(value){
+        this.taskList=this.initialtaskList.filter((x)=>x.title.toLowerCase().includes(value.toLowerCase()));
+      }else{
+        this.taskList=this.initialtaskList;
+      }
+    })
     this.getAllTasks();
   }
   addTask(){
@@ -28,21 +38,21 @@ export class AllTasksComponent {
   }
   getAllTasks(){
     this.httpService.getAllTasks().subscribe((result:any)=>{
-      this.taskList=result;
+      this.initialtaskList=this.taskList=result;
     })
   }
   onComplete(task:any){
     task.complete=true;
     console.log("complete",task);
     this.httpService.updateTask(task).subscribe(()=>{
-      
+      this.getAllTasks();
     })
   }
   onImportant(task:any){
     task.important=true;
     console.log("important",task);
     this.httpService.updateTask(task).subscribe(()=>{
-  
+      this.getAllTasks();
     })
   }
 }
